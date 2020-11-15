a172071b8648b16110808811fbba421bc347487f
JaviMusulman <37247956+JaviMusulman@users.noreply.github.com>
2020-11-15 00:50:50 -0400

    ()-> Commit 11/15 12:49pm +added crochet hook (not fully implemented) +added crochet hook texture +added modtierlist +etc.

--- src/main/java/com/chunkrab/javval/util/RegistryHandler.java
+++ src/main/java/com/chunkrab/javval/util/RegistryHandler.java
@@ -3,7 +3,11 @@ package com.chunkrab.javval.util;
 import com.chunkrab.javval.Javval;
 import com.chunkrab.javval.items.ItemBase;
 import com.chunkrab.javval.items.MagicStrawberry;
+import com.chunkrab.javval.tools.ModItemTier;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.SwordItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -20,4 +24,8 @@ public class RegistryHandler {
     //Items
     public static final RegistryObject<Item> RUBY = ITEMS.register("ruby", ItemBase::new);
     public static final RegistryObject<MagicStrawberry> MAGIC_STRAWBERRY = ITEMS.register("magic_strawberry", MagicStrawberry::new);
+
+    //Tools (ATK SPD IS 4, WHAT GOES INSIDE SWD ITEM() IS A SUBSTRACTOR)
+    public static final RegistryObject<SwordItem> CROCHET_HOOK = ITEMS.register("crochet_hook",()->
+            new SwordItem(ModItemTier.RUBY, 2, -2.4f, new Item.Properties().group(ItemGroup.COMBAT)));
 }
